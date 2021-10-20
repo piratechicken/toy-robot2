ad20b33875305e515fb833f960ef3f4576a1b36d
Simon Dwyer <simon@discolabs.com>
2021-10-20 12:11:22 +1100

    Add brain specs

--- spec/brain_spec.rb
+++ spec/brain_spec.rb
@@ -4,14 +4,28 @@ require 'byebug'
 RSpec.describe ToyRobot::Brain do
   subject { ToyRobot::Brain.new }
 
+  describe '#report' do
+    it 'reports that there is no robot' do
+      expect(subject.report).to eq('No robot placed')
+    end
+
+    context 'when a robot has been placed' do
+      before { subject.place_robot(0, 0, 'SOUTH') }
+
+      it 'reports the status of the robot' do
+        expect(subject.report).to eq('0,0,SOUTH')
+      end
+    end
+  end
+
   describe '#place_robot' do
     let(:x) { 0 }
     let(:y) { 0 }
     let(:facing) { 'NORTH' }
 
-    it 'places the robot' do
-      subject.place_robot(x, y, facing)
+    before { subject.place_robot(x, y, facing) }
 
+    it 'places the robot' do
       expect(subject.report).to eq('0,0,NORTH')
     end
 
@@ -19,8 +33,6 @@ RSpec.describe ToyRobot::Brain do
       let(:x) { 5 }
 
       it 'does not place the robot' do
-        subject.place_robot(x, y, facing)
-
         expect(subject.report).to eq('No robot placed')
       end
     end
@@ -29,14 +41,22 @@ RSpec.describe ToyRobot::Brain do
       let(:facing) { 'north' }
 
       it 'does not place the robot' do
-        subject.place_robot(x, y, facing)
-
         expect(subject.report).to eq('No robot placed')
+      end
+    end
+
+    context 'with multiple placings' do
+      before do
+        placings = [[0, 0, 'NORTH'], [0, 4, 'SOUTH'], [0, 0, 'north']]
+        placings.each { |placing| subject.place_robot(*placing) }
+      end
+
+      it 'finishes up in the final valid placing' do
+        expect(subject.report).to eq('0,4,SOUTH')
       end
     end
   end
 
-  context 'it can be placed several times'
   context 'it ignores other commands prior to valid place'
   context 'report'
   context 'move'
